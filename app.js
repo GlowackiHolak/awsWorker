06e6cb50fd4b377193b547475d283d3f0b865254
Szymon Głowacki <sznglowacki@gmail.com>
2015-02-28 14:45:18 +0100

    completed

---
+++ app.js
@@ -0,0 +1,26 @@
+(function()
+{
+	var helpers = require("./helpers");
+	var Queue = require("queuemanager");
+	var SQSCommand = require("./sqscommand");
+	var Worker = require("./worker");
+	var SQSConsole = require("./sqsconsole");
+	
+	var AWS_CONFIG_FILE = "./config.json";
+	var APP_CONFIG_FILE = "./app.json";
+	
+	var initConsole = function(AWS) {
+		var appConfig = helpers.readJSONFile(APP_CONFIG_FILE);
+		var queue = new Queue(new AWS.SQS(), appConfig.QueueUrl);
+		var sqsCommand = new SQSCommand(queue);
+		new SQSConsole(sqsCommand);
+		
+		var worker = new Worker(sqsCommand, new AWS.S3(), new AWS.SimpleDB());
+		worker.job();
+	}
+	require("./awshelpers").initAWS(initConsole, AWS_CONFIG_FILE);
+	
+})()
+
+
+
